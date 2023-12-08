02d04efdeb1635606c12dd063bbddaec613eec44
tienle1506 <tienlee156@gmail.com>
2023-12-08 14:24:02 +0700

    luong phe duyet ho so

--- hrm/models/__init__.py
+++ hrm/models/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import hr_department
-from . import hr_company
-from . import hr_system
-from . import hr_position
+from . import hr_job
 from . import hr_employee
 from . import hr_users
 from . import hr_approval_flow_config
--- hrm/models/hr_job.py
+++ hrm/models/hr_job.py
@@ -5,19 +5,15 @@ from . import constraint
 
 
 class Position(models.Model):
-    _name = 'hr.position'
-    _description = 'Vị trí'
+    _name = 'hr.job'
+    _description = 'Vị trí công việc'
     _rec_name = "name"
-    _inherit = ['mail.thread', 'mail.activity.mixin', 'utm.mixin']
+    _inherit = ['hr.job', 'mail.thread', 'mail.activity.mixin', 'utm.mixin']
 
-    name = fields.Char(string='Tên Vị Trí', required=True, tracking=True)
     type_block = fields.Selection(selection=[
         (constraint.BLOCK_OFFICE_NAME, constraint.BLOCK_OFFICE_NAME),
-        (constraint.BLOCK_COMMERCE_NAME, constraint.BLOCK_COMMERCE_NAME)], string="Khối", required=True, tracking=True
-        , default=lambda self: self.env.user.block_id)
+        (constraint.BLOCK_COMMERCE_NAME, constraint.BLOCK_COMMERCE_NAME)], string="Khối", required=True, tracking=True)
     # team_type = fields.Selection([('marketing', 'Marketing'), ('sale', 'Sale')], string='Loại đội ngũ',
     #                              default='marketing')
-    active = fields.Boolean(string='Hoạt Động', default=True)
     # related = fields.Boolean(compute='_compute_related_field')
     # check_blocks = fields.Char(default=lambda self: self.env.user.block_id)
-
